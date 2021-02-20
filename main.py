d2a7c879f5e47354284a51f8f7ab93db1b8eca3b
atrox3d <atrox3d@gmail.com>
2021-02-20 05:50:58 +0100

    setup UI

--- main.py
+++ main.py
@@ -1,2 +1,51 @@
+import tkinter
+
 BACKGROUND_COLOR = "#B1DDC6"
 
+
+def right():
+    pass
+
+
+def wrong():
+    pass
+
+
+################################################################################
+#
+#   window
+#
+################################################################################
+window = tkinter.Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+################################################################################
+#
+#   canvas
+#
+################################################################################
+canvas = tkinter.Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+card_front = tkinter.PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 250, image=card_front)
+language_text = canvas.create_text(400, 150, text="Language", fill="black", font=("Ariel", 40, "italic"))
+term_text = canvas.create_text(400, 263, text="Term", fill="black", font=("Ariel", 40, "bold"))
+canvas.grid(row=0, column=0, columnspan=2)
+################################################################################
+#
+#   buttons
+#
+################################################################################
+wrong_image = tkinter.PhotoImage(file="images/wrong.png")
+wrong_button = tkinter.Button(image=wrong_image, highlightthickness=0)
+wrong_button.grid(row=1, column=0)
+#
+right_image = tkinter.PhotoImage(file="images/right.png")
+right_button = tkinter.Button(image=right_image, highlightthickness=0)
+right_button.grid(row=1, column=1)
+
+################################################################################
+#
+#   MAIN LOOP
+#
+################################################################################
+window.mainloop()
