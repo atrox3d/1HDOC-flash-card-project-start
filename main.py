84d6e87dd79748c99f19048191fa272639d25220
atrox3d <atrox3d@gmail.com>
2021-02-20 06:08:30 +0100

    match course names

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter
 
 BACKGROUND_COLOR = "#B1DDC6"
+FONT_ITALIC = font=("Ariel", 40, "italic")
+FONT_BOLD = font=("Ariel", 40, "bold")
 
 
 ########################################################################################################################
@@ -8,13 +10,13 @@ BACKGROUND_COLOR = "#B1DDC6"
 #   functions
 #
 ########################################################################################################################
-def right():
-    print("right")
+def known():
+    print("known")
     pass
 
 
-def wrong():
-    print("wrong")
+def unknown():
+    print("unknown")
     pass
 
 
@@ -31,25 +33,29 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 #   canvas
 #
 ########################################################################################################################
-canvas = tkinter.Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas = tkinter.Canvas(width=800, height=526)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+#   cards
 card_front = tkinter.PhotoImage(file="images/card_front.png")
 card_back = tkinter.PhotoImage(file="images/card_back.png")
-canvas.create_image(400, 250, image=card_front)
-language_text = canvas.create_text(400, 150, text="Language", fill="black", font=("Ariel", 40, "italic"))
-term_text = canvas.create_text(400, 263, text="Term", fill="black", font=("Ariel", 40, "bold"))
+canvas.create_image(400, 263, image=card_front)
+#   texts
+language_text = canvas.create_text(400, 150, text="Language", fill="black", font=FONT_ITALIC)
+term_text = canvas.create_text(400, 263, text="Term", fill="black", font=FONT_BOLD)
+#
 canvas.grid(row=0, column=0, columnspan=2)
 ########################################################################################################################
 #
 #   buttons
 #
 ########################################################################################################################
-wrong_image = tkinter.PhotoImage(file="images/wrong.png")
-wrong_button = tkinter.Button(image=wrong_image, highlightthickness=0, command=right)
-wrong_button.grid(row=1, column=0)
+unknown_image = tkinter.PhotoImage(file="images/wrong.png")
+unknown_button = tkinter.Button(image=unknown_image, highlightthickness=0, command=unknown)
+unknown_button.grid(row=1, column=0)
 #
-right_image = tkinter.PhotoImage(file="images/right.png")
-right_button = tkinter.Button(image=right_image, highlightthickness=0, command=right)
-right_button.grid(row=1, column=1)
+known_image = tkinter.PhotoImage(file="images/right.png")
+known_button = tkinter.Button(image=known_image, highlightthickness=0, command=known)
+known_button.grid(row=1, column=1)
 
 ########################################################################################################################
 #
